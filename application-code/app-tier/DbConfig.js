d6a549b17e0483d07a72ac76ed1e3a2202fdb45c
iam-Lahari <laharireddykudala@gmail.com>
2026-08-29 08:42:54 +0530

    DbConfig.js modified

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'database-1.cv62u644qrvz.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'YourPassword',
+    DB_PWD : 'Lahari12345',
     DB_DATABASE : 'webappdb'
 });
